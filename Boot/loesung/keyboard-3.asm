8746e0091465d3c7059d5c2fa2aa0d2fe066d1a0
Stefan Reimbold <Stefan.Reimbold@de.ibm.com>
2025-06-27 11:12:22 +0200

    add comments

--- Boot/loesung/keyboard-3.asm
+++ Boot/loesung/keyboard-3.asm
@@ -1,20 +1,18 @@
-;boot.asm
+;keyboard.asm
 [ORG 0x7C00]
-    xor ax, ax
-    mov ds, ax
+    xor ax, ax                      ; make it zero
+    mov ds, ax                      ; data segment starts at zero
     mov ss, ax
     mov sp, 0x9C00
 
-    cli
-    mov bx, 0x09
+    cli                             ; disable interrupts
+    mov bx, 0x09                    ; set interrupt number
     shl bx, 2
     xor ax, ax
     mov gs, ax
     mov [gs:bx], word keyhandler    ; register keyboard irpt handler
     mov [gs:bx+2], ds
-    sti
-
-    ;go into endless loop
+    sti                             ; enable interrupts
 
 ;------------------------------------
 hang:
@@ -49,13 +47,13 @@ keyhandler:
     mov  word[reg16], ax
     call printreg16
 
-    mov ax, 0x20
+    mov ax, 0x20                    ; print one blank
     call cprint
-    mov bx, [port60]
-    mov cx, [keymap]
+    mov bx, [port60]                ; get char out of keymap string
+    mov cx, [keymap]                ; by index
     add bx, cx
     mov ax, [bx]
-    call cprint
+    call cprint                     ; print that char
 
     mov  si, lineend                ; print CR/LF
     call sprint
@@ -77,20 +75,20 @@ done:
 ;------------------------------------
 dochar:
     call cprint
-sprint:
+sprint:                             ; print string
     lodsb
     cmp al, 0
     jne dochar
     ret
 
-cprint:
+cprint:                             ; print character
     mov ah, 0x0E
     int 0x10
     ret
 
 ;------------------------------------
-printreg16:
-    mov di, outstr16
+printreg16:                         ; print 16-bit hex string
+    mov di, outstr16                ; write into outstr16
     mov ax, [reg16]
     mov si, hexstr
     mov cx, 4
@@ -104,7 +102,7 @@ hexloop:
     dec cx
     jnz hexloop
 
-    mov si, outstr16
+    mov si, outstr16                ; print outstr16
     call sprint
 
     ret
@@ -122,7 +120,7 @@ lineend     db 13, 10, 0
 
 ;------------------------------------
     times 510-($-$$) db 0
-    db 0x55
-    db 0xAA
+    db 0x55                         ; write boot sector signature at
+    db 0xAA                         ; and of sector
 
 ;------------------------------------
