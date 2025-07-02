47aabc25a9b5e7a19e5dddb0e5507e0fa31b4c32
MartialElch <stefan.reimbold@imail.de>
2025-07-02 18:05:50 +0200

    add shell

--- OS16C/loesung/OS16C-3/include/system.h
+++ OS16C/loesung/OS16C-3/include/system.h
@@ -9,12 +9,15 @@ typedef unsigned char       uint8_t;
 typedef unsigned short int  uint16_t;
 
 /* irq */
-uint8_t inb(uint16_t port);
-void outb(uint8_t v, uint16_t port);
+extern uint8_t inb(uint16_t port);
+extern void outb(uint8_t v, uint16_t port);
 extern void registerinterrupt(int irq, void* handler);
 
 /* keyboard */
 extern void keyboardhandler(void);
+
+/* shell */
+extern void shell(void);
 
 /* terminal */
 extern void print(char *s);
