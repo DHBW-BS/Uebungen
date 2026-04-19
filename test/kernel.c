ad38469bf74b3d4572e88e62a997bf27b48b6625
MartialElch <stefan.reimbold@imail.de>
2026-04-19 17:06:29 +0200

    Update kernel.c

--- test/kernel.c
+++ test/kernel.c
@@ -20,7 +20,7 @@ int kmain(void) {
     print("starting kernel ...\r\n");
 
     registerinterrupt(9, keyboardhandler);
-	registerinterrupt(0x21, sys_syscall);
+	registerinterrupt(0x21, syscall);
 
     shell();
     halt();
