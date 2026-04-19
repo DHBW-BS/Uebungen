b64214cbb6268b284f70c78e10e12c9c4705a248
MartialElch <stefan.reimbold@imail.de>
2026-04-19 12:23:54 +0200

    Update shell.c

--- test/shell.c
+++ test/shell.c
@@ -257,6 +257,8 @@ void shell(void) {
 	static char s[80];
 	char c;
 
+	dump_floppy(0);
+
 	int i = 0;
 	s[0] = '\0';
 
@@ -285,8 +287,6 @@ int strcmp(const char *s1, const char *s2) {
 	char c;
 	int rc = 0;
 	int i=0;
-
-	dump_floppy(0);
 
 	while((c = s1[i]) != '\0') {
 		if (s2[i] == '\0') {
